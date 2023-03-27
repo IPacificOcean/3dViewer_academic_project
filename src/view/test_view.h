8760c16a22d94b4f947864572142df623e41a517
violator <violator@student.21-school.ru>
2023-03-27 19:09:40 +0300

    view

---
+++ src/view/test_view.h
@@ -0,0 +1,12 @@
+//
+// Created by Violator Emilie on 3/27/23.
+//
+
+#ifndef INC_3DVIEWER_V2_0_SRC_VIEW_TEST_VIEW_H_
+#define INC_3DVIEWER_V2_0_SRC_VIEW_TEST_VIEW_H_
+
+class test_view {
+
+};
+
+#endif //INC_3DVIEWER_V2_0_SRC_VIEW_TEST_VIEW_H_
