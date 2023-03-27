3378948a123c9ff07fe113538d2a89a0f7e0de11
violator <violator@student.21-school.ru>
2023-03-27 17:33:38 +0300

    struct

---
+++ src/controller/controller.cc
@@ -0,0 +1,5 @@
+//
+// Created by Violator Emilie on 3/27/23.
+//
+
+#include "controller.h"
---
+++ src/model/kernel/parser.h
@@ -0,0 +1,12 @@
+//
+// Created by Violator Emilie on 3/27/23.
+//
+
+#ifndef INC_3DVIEWER_V2_0_SRC_MODEL_KERNEL_PARSER_H_
+#define INC_3DVIEWER_V2_0_SRC_MODEL_KERNEL_PARSER_H_
+
+class Parser {
+
+};
+
+#endif //INC_3DVIEWER_V2_0_SRC_MODEL_KERNEL_PARSER_H_
