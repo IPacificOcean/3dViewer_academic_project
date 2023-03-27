e4ed076c2ceb4254d7ece2f85719ea3000f0cb8c
violator <violator@student.21-school.ru>
2023-03-27 17:57:36 +0300

    test

--- src/tests/tests.cc
+++ src/tests/tests.cc
@@ -2,3 +2,23 @@
 // Created by Violator Emilie on 3/27/23.
 //
 
+#include <gtest/gtest.h>
+
+#include <iostream>
+
+
+using std::cout;
+using std::endl;
+
+class ModelTest : public ::testing::Test {
+ protected:
+  void SetUp() override {}
+
+};
+
+TEST_F(ModelTest, test) {
+
+cout << "first try" << endl;
+
+
+}
