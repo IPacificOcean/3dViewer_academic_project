810ce1cb56f492609902dc9999da49fcd49a07d9
violator <violator@student.21-school.ru>
2023-03-27 17:33:38 +0300

    struct

---
+++ src/model/dao/data_model.h
@@ -0,0 +1,12 @@
+//
+// Created by Violator Emilie on 3/27/23.
+//
+
+#ifndef INC_3DVIEWER_V2_0_SRC_MODEL_DAO_DATA_MODEL_H_
+#define INC_3DVIEWER_V2_0_SRC_MODEL_DAO_DATA_MODEL_H_
+
+class DataModel {
+
+};
+
+#endif //INC_3DVIEWER_V2_0_SRC_MODEL_DAO_DATA_MODEL_H_
---
+++ src/model/facade.h
@@ -0,0 +1,12 @@
+//
+// Created by Violator Emilie on 3/27/23.
+//
+
+#ifndef INC_3DVIEWER_V2_0_SRC_MODEL_FACADE_H_
+#define INC_3DVIEWER_V2_0_SRC_MODEL_FACADE_H_
+
+class Facade {
+
+};
+
+#endif //INC_3DVIEWER_V2_0_SRC_MODEL_FACADE_H_
