09712c1dd2487ad92a6fb65a08d6a277268d93ba
tysaneno <tysaneno@student.21-school.ru>
2023-03-28 17:37:37 +0300

    data model singleton

--- src/model/facade.h
+++ src/model/facade.h
@@ -2,43 +2,43 @@
 // Created by Violator Emilie on 3/27/23.
 //
 
-
 #ifndef INC_3DVIEWER_V2_0_SRC_MODEL_FACADE_H_
 #define INC_3DVIEWER_V2_0_SRC_MODEL_FACADE_H_
 
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <string>
-#include "./observer/observer.h"
-#include "./observer/observable.h"
+
 #include "./dao/data_model.h"
+#include "./observer/observable.h"
+#include "./observer/observer.h"
 
 namespace s21 {
 
 class Facade : public Observable {
  public:
-  Facade()  {}
+  Facade() {}
   void test() {
-	std::string line;
-	std::ifstream testfile("/Users/violator/github/3DViewer_v2.0/src/model/test.txt");
-	if (testfile.is_open()) {
-	  while (getline(testfile, line)) {
-		std::cout << line << '\n';
-	  }
-	  testfile.close();
-	} else {
-	  std::cout << "Unable to open file";
-	}
+    std::string line;
+    std::ifstream testfile(
+        "/Users/violator/github/3DViewer_v2.0/src/model/test.txt");
+    if (testfile.is_open()) {
+      while (getline(testfile, line)) {
+        std::cout << line << '\n';
+      }
+      testfile.close();
+    } else {
+      std::cout << "Unable to open file";
+    }
   }
 
   void changeColor() {
-//	this->notifyUpdate(vertex_);
+    //	this->notifyUpdate(vertex_);
   }
 
  private:
-
 };
 
-}
+}  // namespace s21
 
-#endif //INC_3DVIEWER_V2_0_SRC_MODEL_FACADE_H_
+#endif  // INC_3DVIEWER_V2_0_SRC_MODEL_FACADE_H_
