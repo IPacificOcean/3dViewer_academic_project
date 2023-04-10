cf5bdc8afe44bc451ca620759ea79f445af4ca05
tysaneno <tysaneno@student.21-school.ru>
2023-04-10 20:06:32 +0300

    test ci_cd_5

--- src/telegram_bot.sh
+++ src/telegram_bot.sh
@@ -14,7 +14,7 @@ else
 fi
 
 URL="https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage"
-TEXT="Job name: $CI_JOB_NAME%0A%0AStatus: $1%0A%0AProject:+$CI_PROJECT_NAME%0AURL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG"
+TEXT="Job name: $CI_JOB_NAME%0A%0AStatus: $STATUS%0A%0AProject:+$CI_PROJECT_NAME%0AURL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG"
 
 curl -s --max-time $TIME -d "chat_id=$TELEGRAM_USER_ID_TYSANENO&disable_web_page_preview=1&text=$TEXT" $URL > /dev/null
 curl -s --max-time $TIME -d "chat_id=$TELEGRAM_USER_ID_VIOLATOR&disable_web_page_preview=1&text=$TEXT" $URL > /dev/null
